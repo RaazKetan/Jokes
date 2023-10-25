f50b48365b824daddf28cddf08c26ea63d5abd8d
Raaz <21ketanraaz@gmail.com>
2023-10-25 20:00:19 +0530

    Final Commit

--- script.js
+++ script.js
@@ -1,10 +1,15 @@
 const jokeContainer = document.getElementById("joke");
 const btn = document.getElementById("btn");
-const url = "https://v2.jokeapi.dev/joke/Any";
+const url = "https://v2.jokeapi.dev/joke/Any?type=single";
 
 let getJoke = () =>{
+    jokeContainer.classList.remove("fade");
    fetch(url)
    .then(data=>data.json())
-   .then(item => console.log(item.joke));
+   .then(item => {
+    jokeContainer.textContent = `${item.joke}`;
+    jokeContainer.classList.add("fade");
+   });
 }
+btn.addEventListener("click",getJoke);
 getJoke();
